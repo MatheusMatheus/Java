713ce1b5ab659183cfa0782d08ed0ad35e58f077
matheus.rodrigues <matheus.rodrigues@foton001214.foton.local>
2018-10-15 17:33:38 -0300

    Segundo commit

--- OLX/src/main/java/br/com/matheus/modelo/Cliente.java
+++ OLX/src/main/java/br/com/matheus/modelo/Cliente.java
@@ -13,42 +13,53 @@ public class Cliente implements Serializable {
 	private String nome;
 	private String email;
 	private String telefone;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public Localizacao getLocalizacao() {
 		return localizacao;
 	}
+
 	public void setLocalizacao(Localizacao localizacao) {
 		this.localizacao = localizacao;
 	}
+
 	public Login getLogin() {
 		return login;
 	}
+
 	public void setLogin(Login login) {
 		this.login = login;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	
+
 }
