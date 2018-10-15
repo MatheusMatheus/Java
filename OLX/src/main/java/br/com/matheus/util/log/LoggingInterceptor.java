a3a2d50377f951ba76aa9f79d9617d19cf6ac1c3
matheus.rodrigues <matheus.rodrigues@foton001214.foton.local>
2018-10-15 17:35:46 -0300

    Terceiro commit

---
+++ OLX/src/main/java/br/com/matheus/util/log/LoggingInterceptor.java
@@ -0,0 +1,37 @@
+package br.com.matheus.util.log;
+
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundConstruct;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+@Interceptor
+@Loggable
+public class LoggingInterceptor {
+
+	@Inject
+	private Logger logger;
+	
+	@AroundConstruct
+	private void init(InvocationContext invocationContext) throws Exception {
+		logger.fine("Entrando no construtor");
+		try {
+			invocationContext.proceed();
+		} finally {
+			logger.fine("Saindo do construtor");
+		}
+	}
+	
+	@AroundInvoke
+	public Object logMethod(InvocationContext ic) throws Exception {
+		logger.entering(ic.getTarget().toString(), ic.getMethod().toString());
+		try {
+			return ic.proceed();
+		} finally {
+			logger.exiting(ic.getTarget().toString(), ic.getMethod().toString());
+		}
+	}
+}
